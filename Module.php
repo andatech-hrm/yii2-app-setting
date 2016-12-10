b82d2c22efe8905ff0647779ba86b29848cbd817
kuakling <kuakling@gmail.com>
2016-12-10 05:18:37 -0500

    gii crud

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace andahrm\setting;
+
+/**
+ * setting module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'andahrm\setting\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
